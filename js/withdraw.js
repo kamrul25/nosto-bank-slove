aff4fc1ae0f4bdb347a0ef474b61823b1e7a3612
kamrul25 <kamrul.coder01@gmail.com>
2023-03-09 07:48:01 +0600

    final commit

--- js/withdraw.js
+++ js/withdraw.js
@@ -11,10 +11,16 @@
 
 document.getElementById("btn-withdraw").addEventListener("click", function () {
   const newWithdrawAmount = getInputFieldValueById("withdraw-field");
-  const previousWithdrawTotal = getTextElementValueById("withdraw-total ");
+  const previousWithdrawTotal = getTextElementValueById("withdraw-total");
   const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-  setTextElementValueById(withdraw - total, newWithdrawTotal);
+  
   const previousBalanceTotal = getTextElementValueById("balance-total");
-  const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-  setTextElementValueById("balance-total", newBalanceTotal);
+
+  if(newWithdrawTotal <= previousBalanceTotal){
+    setTextElementValueById("withdraw-total", newWithdrawTotal);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawTotal;
+    setTextElementValueById("balance-total", newBalanceTotal);
+  }else{
+    alert("You cannot withdraw money more than your balance. Tor bapper bank ae ato taka nai!!! Tore ami tejo korlam");
+  }
 });
